0af6ddeebc0f4ace2b22d9aeb2685681c28b2e2a
Tusshar Lingagiri <2909264l@student.gla.ac.uk>
2025-02-06 11:54:48 +0000

    This is Chapter 8 done

--- rango/views.py
+++ rango/views.py
@@ -17,6 +17,8 @@ def index(request):
     return render(request, 'rango/index.html', context=context_dict)
 
 def about(request):
+    print(request.method)
+    print(request.user)
     return render(request, 'rango/about.html')
 
 def show_category(request, category_name_slug):
